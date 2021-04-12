b8616f13903b6bce10ca3464c5a02657b763edd8
Malespero <a73914862@yandex.ru>
2021-04-12 19:22:27 +0700

    validate?

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,4 +1,11 @@
-class Diary < ActiveRecord::Base
+class Diary < ApplicationRecord
   has_many :notes
   enum kind: [:pub, :priv]
+
+  validates :expiration, presence: true, if: :isPrivate?
+
+  def isPrivate?
+    kind == "priv"
+  end
+
 end
